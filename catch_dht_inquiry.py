05e49845f37cca5258d0188e2f529c4efaf32c59
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-07-28 15:32:44 +0200

    Create files

---
+++ catch_dht_inquiry.py
@@ -0,0 +1,55 @@
+import json
+
+import websocket
+
+import classification_server
+
+
+def on_message(ws, message):
+    print("Received:")
+
+    json_message = json.loads(message)
+
+    if "Persistent" in json_message:
+        json_message = json_message["Persistent"]
+        topic_name = json_message["topic_name"]
+        # handle topic name
+        if topic_name == "SIFIS:Privacy_Aware_Device_DHT_inquiry":
+            if "value" in json_message:
+                json_message = json_message["value"]
+                dictionary = json_message["Dictionary"]
+                requestor_id = json_message["requestor_id"]
+                request_id = json_message["request_id"]
+                dictionary = dictionary.split("defaultdict(<class 'int'>, ", 1)[1]
+                data = {
+                    "requestor_id": requestor_id,
+                    "request_id": request_id,
+                    "dictionary": dictionary,
+                }
+                print(data)
+                response = classification_server.receive_data(data)
+                print(response)
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("### Connection closed ###")
+
+
+def on_open(ws):
+    print("### Connection established ###")
+
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp(
+        "ws://localhost:3000/ws",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close,
+    )
+
+    ws.run_forever()
---
+++ classification_server.py
@@ -0,0 +1,70 @@
+import json
+
+import joblib
+
+import send_results
+
+MODEL_PATH = "model.joblib"
+
+
+def transform_json_to_instance(json_data):
+    # Converti la stringa JSON in un dizionario Python
+    data_dict = json.loads(json_data)
+    # data_dict = eval(data_dict)
+    # Definisci l'ordine degli attributi
+    attributes = [
+        "domo_ble_thermometer",
+        "shelly_1plus",
+        "domo_switch",
+        "shelly_em",
+        "domo_power_energy_sensor",
+        "shelly_25",
+        "domo_light",
+        "shelly_1pm",
+        "domo_thermostat",
+    ]
+    # Crea una lista vuota per l'istanza
+    instance = []
+    # Per ogni attributo nell'ordine definito sopra, aggiungi il valore corrispondente alla lista
+    for attribute in attributes:
+        value = data_dict.get(attribute, 0)
+        instance.append(value)
+    # Restituisci l'istanza come lista
+    return instance
+
+
+def predict_instance(model_path, instance_json):
+    # instance = transform_json_to_instance(instance_json)
+    model = joblib.load(model_path)
+    prediction = model.predict([instance_json])
+    return prediction[0]
+
+
+def receive_data(data):
+    data = eval(data)
+    request_id = data.get("request_id", None)
+    requestor_id = data.get("requestor_id", None)
+    dictionary = data.get("dictionary", None)
+    try:
+        dictionary = dictionary.replace(")", "")
+        dictionary = dictionary.replace("'", '"')
+    except:
+        pass
+    print("Received: " + str(dictionary))
+    instance = transform_json_to_instance(dictionary)
+    prediction = predict_instance(MODEL_PATH, instance)
+    {"instance": data, "prediction": str(prediction)}
+    for elem in str(prediction):
+        if elem == "1":
+            response = "System Violation"
+            send_results.send_results(
+                request_id, requestor_id, str(dictionary), response
+            )
+            return "System Violation"
+        elif elem == "0":
+            response = "Correct Invocation"
+            send_results.send_results(
+                request_id, requestor_id, str(dictionary), response
+            )
+            print("Correct Invocation")
+            return "Correct Invocation"
